d63e7761c9536967b1b031d9ced2f8e5db8ef9ad
Jorgebpinedo <j.bpinedo@alumnos.upm.es>
2023-04-01 18:18:43 +0200

    codigo cliente en clases
    codigo cliente metido en clases (falta tratamiento)

--- Proyecto_Cpp/Cliente.h
+++ Proyecto_Cpp/Cliente.h
@@ -1,3 +1,38 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+class cliente
+{
+private:
+    int argc;
+    char** argv;
+
+    addrinfo* result = nullptr;
+    addrinfo* ptr = nullptr;
+    addrinfo hints;
+
+    int iResult;
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+public:
+    void creaSocket();
+    void conecta();
+};
 
