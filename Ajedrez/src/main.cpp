e4e28ef90847b5442a24da6a64f35d02d6e80062
Vegely <bogurad.baranski@alumnos.upm.es>
2023-05-03 17:10:25 +0200

    Gitignore funcion? :DDDDDDDDDDD

--- Ajedrez/src/main.cpp
+++ Ajedrez/src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
 	tablero.imprimeTablero();
 	////////////////////////////////////////////////////////////
 	
-	std::cout << "sopdjasopk";
+	std::cout << "1";
 
 	return 0;
 }
