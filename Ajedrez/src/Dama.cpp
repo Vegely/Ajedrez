3626c73010765feb4a7e82ae84d76993cc7d643c
Vegely <bogurad.baranski@alumnos.upm.es>
2023-05-23 13:23:01 +0200

    Arreglada dama

--- Ajedrez/src/Dama.cpp
+++ Ajedrez/src/Dama.cpp
@@ -9,16 +9,7 @@ DatosClavada Dama::actualizarVariables(bool clavada, Posicion direccionClavada, 
 	DatosClavada piezaClavada;
 	Posicion posicionAux;
 	bool piezaEncontrada = false; //Variable para comprobar si se ha topado con una pieza
-<<<<<<< Updated upstream
-	
-	std::vector<Posicion> direcciones;
 
-	if (!clavada) //Clavar el movimiento de la pieza
-		direcciones = { Posicion(-1, 0), Posicion(1, 0), Posicion(0, -1), Posicion(0, 1),		//Direcciones paralelas
-				Posicion(-1, -1), Posicion(1, -1), Posicion(-1, 1), Posicion(1, 1) };		//Direcciones diagonales
-	else
-		direcciones = { direccionClavada, -direccionClavada };
-=======
 	std::vector<Posicion> direcciones;
 
 	if (!clavada) //Clavar el movimiento de la pieza
@@ -30,7 +21,6 @@ DatosClavada Dama::actualizarVariables(bool clavada, Posicion direccionClavada, 
 	{
 		direcciones = { direccionClavada,-direccionClavada };
 	}
->>>>>>> Stashed changes
 
 	for (Posicion direccion : direcciones)
 	{
