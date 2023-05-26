b1ba7eb028da5ff46e89691a22854f09d69d3a38
JuanNicolasJimenez <79970058+JuanNicolasJimenez@users.noreply.github.com>
2023-05-26 18:07:57 +0200

    force push - prototipo final

--- Ajedrez/src/Pieza.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pieza.h"
-#include "Tablero.h"
-
-void Pieza::clearVariables()
-{
-	puede_mover.clear();
-	puede_comer.clear();
-}
-
--- Ajedrez/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Callbacks.h"
-
-int main(int argc, char* argv[])
-{
-	graphicsInit(&argc, argv); // Inicializacion del motor grafico
-
-	return 0;
-}
