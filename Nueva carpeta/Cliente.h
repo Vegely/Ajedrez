dff7ee8fcd789207396cae568c72b70143cb706c
Jorgebpinedo <j.bpinedo@alumnos.upm.es>
2023-06-15 22:51:33 +0200

    redes

---
+++ Nueva carpeta/Cliente.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "Socket.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+
+class Cliente
+{
+private:
+    //Se encarga de la comunicación
+    Socket* sck = nullptr;
+
+    //Guarda la dirección ip del host
+    std::string ipv4 = "";//"192.168.0.21";
+public:
+    //Constructor:
+    // - Inicialización por defecto
+    // - Inicialización Winsock
+    Cliente() { };
+
+    //No hace falta destructor (conflictos de copia/asignación eliminados), lo gestiona el SO
+
+    //Eliminamos posibles conflictos de copia/asignación
+    Cliente(const Cliente&) = delete;
+    Cliente& operator= (const Cliente&) = delete;
+
+    void inicializa() { inicializaWinSock(); };
+    //Conecta el cliente
+    bool conectarCliente();
+    //Desconecta el cliente
+    void desconectarCliente();
+
+    //Envía una cadena de caracteres al servidor
+    //@param std::string s: cadena a enviar
+    //@return Número de bytes enviados
+    int enviarAServidor(std::string s);
+    //Recibe una cadena de caracteres del servidor
+    //@param std::string& s: cadena recibida
+    //@return Número de bytes recibidos
+    int recibirDeServidor(std::string& s);
+
+    std::string& getIp() { return ipv4; };
+};
+
+// Main (poner la dirección ip que aparece en la pantalla del servidor en el campo ipv4 del cliente)
+
+/*
+
+void cambiarTurno(bool& tnegras, bool& tblancas) {
+    tnegras = !tnegras;
+    tblancas = !tblancas;
+}
+
+    //Servidor: negras
+    //Cliente: blancas
+    std::string cenviar = "", crecibir = "";
+    bool tblancas = 0, tnegras = 1;
+    bool salir = 0, cambiar = 0;
+
+
+    Cliente c;
+
+    c.conectarCliente();
+
+    while (!salir) {
+        if (tnegras) {
+            if (c.recibirDeServidor(crecibir) > 0) {
+                std::cout << crecibir << std::endl;
+                crecibir = "";
+            }
+            cambiarTurno(tnegras, tblancas);
+        }
+        else if (tblancas) {
+            cenviar = "";
+            std::getline(std::cin, cenviar);
+            c.enviarAServidor(cenviar);
+            cambiarTurno(tnegras, tblancas);
+        }
+
+    }
+
+    c.desconectarCliente();
+
+*/
---
+++ Nueva carpeta/Socket.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+constexpr int MAX_LONG_BUFF = 256;
+//Puerto en el que se realiza la conexión por defecto
+constexpr const char* PUERTO_PREDETERMINADO = "42069";
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+class Socket
+{
+private:
+	//Almacena parámetros de la red
+	addrinfo* red_info = nullptr;
+	//Almacena información de la conexión que acepta el host
+	addrinfo* host_info = nullptr;
+	//Servidor: comunicación y acepar peticiones de conexión
+	//Cliente: comunicación
+	SOCKET sck = INVALID_SOCKET;
+
+public:
+	//Inicialización por defecto
+	Socket() {};
+	//Inicialización
+	//@param int flags: servidor --> AI_PASSIVE, cliente --> NULL
+	//@param int familia: familia del protocolo (AF_INET para familia IPv4)
+	//@param int tipo_socket: tipo de socket a usar (SOCK_STREAM por defecto)
+	//@param int protocolo: protocolo usado (IPPROTO_TCP para TCP)
+	Socket(int flags, int familia = AF_INET,
+		int tipo_socket = SOCK_STREAM, int protocolo = IPPROTO_TCP);
+
+	//No defino destructor porque no existe posibilidad de copia/asignación (no hay conflicto de recursos)
+	//Además, las estructuras de datos dentro de Socket son complejas y no están orientadas a objetos (no tienen destructor),
+	//por lo que es mejor dejar al SO que se encargue de la gestión de dicha memoria.
+
+	//No se pueden copiar ni asignar los sockets
+	Socket(const Socket&) = delete;
+	Socket& operator= (const Socket&) = delete;
+
+	//Crea un socket asociado a una dirección IP
+	//@param const char* direccion: dirección IP del host (nullptr si lo usamos desde el servidor)
+	void creaSocket(const char* direccion);
+	//Vincula el programa con el socket creado
+	void vincula();
+	//Atiende peticiones de conexión del cliente
+	void escucha();
+	//Acepta peticiones de conexión del cliente
+	//Cierra el socket que acepta las conexiones (solo hay 1 conexión) y crea un socket de comunicación
+	//@param Socket& sck_aux: socket para la comunicación con el cliente
+	void aceptaConexion(Socket& sck_aux);
+	//Conecta el cliente con el servidor
+	bool conectarAServidor();
+	//Envía una cadena de caracteres al cliente/servidor
+	//@param std::string s: cadena de caracteres a enviar
+	//@return  Número de bytes enviados
+	int envia(std::string s);
+	//Recibe una caddena de caracteres del cliente/servidor
+	//@param std::string& s: cadena de caracteres recibida
+	//@return Número de bytes recibidos
+	int recibe(std::string& s);
+
+	//Desconecta el cliente/servidor: termina la conexión y cierra los sockets
+	void desconecta();
+};
+
+//Inicializa la librería WinSock
+void inicializaWinSock();
+
+
+
